72d087bc7abaab5239037698ea098aed97e501b0
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-06-03 10:20:42 +0530

    Update employee.java

--- encapsulation/employee.java
+++ encapsulation/employee.java
@@ -1,1 +1,22 @@
+package encapsulation;
+
+public class employee {
+	private int salary ;
+	private String name;	
+	
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getName1() {
+		return name;
+	}
+	public void setName1(String name) {
+		this.name = name;
+	}
+	
+	
+}
 
