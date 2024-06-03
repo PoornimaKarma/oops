3fe6bd8f0d600a9ae026e33c4b4b86b7965773ac
PoornimaKarma <161825445+PoornimaKarma@users.noreply.github.com>
2024-06-03 10:18:15 +0530

    Update employee.java

--- abstraction/employee.java
+++ abstraction/employee.java
@@ -1,1 +1,16 @@
-
+package abstraction;
+	abstract class Bike
+	   {  
+		  abstract void run();  
+		}  
+		class Honda extends Bike{  
+		void run()
+		{
+			System.out.println("running safely");
+		}  
+		public static void main(String args[])
+		{  
+		 Bike obj = new Honda();  
+		 obj.run();  
+		}  
+		}  
